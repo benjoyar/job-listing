51f17e5edbb5c789bd25fa2b0f4f2577ecddf2e6
zhongxh <zhxhyz@gmail.com>
2017-04-15 15:29:42 +0800

    implement job#show

--- app/controllers/jobs_controller.rb
+++ app/controllers/jobs_controller.rb
@@ -1,11 +1,15 @@
 class JobsController < ApplicationController
-  
+
   def index
     @jobs = Job.all
   end
 
   def new
     @job = Job.new
+  end
+
+  def show
+    @job = Job.find(params[:id])
   end
 
   def create
