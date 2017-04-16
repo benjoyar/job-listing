eb589fba2f544a964fab3117f24488f68d045635
zhongxh <zhxhyz@gmail.com>
2017-04-16 10:34:05 +0800

    delete edit_&_delete_btn & add release time

--- app/controllers/jobs_controller.rb
+++ app/controllers/jobs_controller.rb
@@ -1,7 +1,7 @@
 class JobsController < ApplicationController
   before_action :authenticate_user!, only: [:new, :create, :update, :edit, :destroy]
   def index
-    @jobs = Job.all
+    @jobs = Job.all.recent
   end
 
   def new
--- app/models/job.rb
+++ app/models/job.rb
@@ -1,4 +1,5 @@
 class Job < ApplicationRecord
   belongs_to :user
   validates :title, presence: true
+  scope :recent, -> { order("created_at DESC")}
 end
