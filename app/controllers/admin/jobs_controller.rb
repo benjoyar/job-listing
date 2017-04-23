a75b8b565bb1a7cf875ec558fa951c7a536b7960
zhongxh <zhxhyz@gmail.com>
2017-04-23 22:20:31 +0800

    implement jobs pagination with will_paginate

--- app/controllers/admin/jobs_controller.rb
+++ app/controllers/admin/jobs_controller.rb
@@ -12,7 +12,7 @@ class Admin::JobsController < ApplicationController
   end
 
   def index
-    @jobs = Job.all
+    @jobs = Job.all.recent.paginate(:page => params[:page], :per_page => 5)
   end
 
   def create
